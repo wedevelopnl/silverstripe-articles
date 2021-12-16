b2d0a93dd7ffc3a88bf5eaa56fe47b4eed4c7b4f
Robert van Lienden <robert_1337@live.nl>
2021-12-16 12:06:35 +0100

    Added controller for ArticlePage

---
+++ src/Controllers/ArticlePageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TheWebmen\Articles\Controllers;
+
+use TheWebmen\Articles\Pages\ArticlePage;
+
+/**
+ * Class ArticlePageController
+ * @package TheWebmen\Articles\Controllers
+ *
+ * @method ArticlePage data()
+ */
+class ArticlePageController extends \PageController
+{
+
+}
